13707026862590fe715fade98de299bd62a17c3d
Dave Waters <davewaters@live.ca>
2017-01-14 01:11:58 -0500

    Update helper.js
    Edited comments

--- frontend-nanodegree-resume-master/js/helper.js
+++ frontend-nanodegree-resume-master/js/helper.js
@@ -1,14 +1,5 @@
 /*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
+These are the HTML strings used to create the detail section of the resume page. 
 */
 var HTMLprependHeader = {
     name: '<h1 id="name">%data%</h1>',
@@ -83,45 +74,12 @@ var HTMLeducation = {
 var googleMap = '<div id="map"></div>';
 
 /*
-The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
-$(document).ready(function() {
-    $('button').click(function() {
-        var $name = $('#name');
-        var iName = inName($name.text()) || function() {};
-        $name.html(iName);
-    });
-});
-
-/*
-The next few lines about clicks are for the Collecting Click Locations quiz in the lesson Flow Control from JavaScript Basics.
-*/
-var clickLocations = [];
-
-function logClicks(x, y) {
-    clickLocations.push({
-        x: x,
-        y: y
-    });
-    console.log('x location: ' + x + '; y location: ' + y);
-}
-
-$(document).click(function(loc) {
-    // your code goes here!
-});
-
-
-
-/*
-This is the fun part. Here's where we generate the custom Google Map for the website.
-See the documentation below for more details.
-https://developers.google.com/maps/documentation/javascript/reference
+This code generates the custom Google Map for the website.
 */
 var map; // declares a global map variable
 
-
 /*
-Start here! initializeMap() is called when page is loaded.
+InitializeMap() is called when page is loaded.
 */
 function initializeMap() {
 
